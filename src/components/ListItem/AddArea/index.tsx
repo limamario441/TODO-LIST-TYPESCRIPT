e0056468957559523a56fcc80b99c9af51581ff3
Mario Lima <limamario441@gmail.com>
2023-03-11 12:02:17 -0300

    todo

---
+++ src/components/ListItem/AddArea/index.tsx
@@ -0,0 +1,37 @@
+import { useState, KeyboardEvent } from 'react';
+import * as C from './styles';
+
+
+type Props ={
+
+    onEnter: (taskName: string) => void
+}
+
+export const AddArea = ({onEnter}: Props) => {
+
+const [inputText, setInputText] = useState('');
+
+const handlekeyUp = (e: KeyboardEvent) =>{
+
+    if(e.code === 'Enter' && inputText !== ''){
+   
+        onEnter(inputText);
+        setInputText('');
+
+    }
+
+}
+  return (
+    <C.Container>
+   
+   <div className="image">+</div>
+   <input
+    type="text"
+     placeholder='Adicione um Curso' 
+     value={inputText}
+     onChange={e=>setInputText(e.target.value)}
+     onKeyUp={handlekeyUp}
+/>
+    </C.Container>
+  );
+}
